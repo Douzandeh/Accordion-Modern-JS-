949b5051c4217c1190bdbda4889e8c27f0103351
Hossein <hosseindozandeh799@gmail.com>
2023-01-06 01:07:53 +0330

    053

--- script.js
+++ script.js
@@ -17,22 +17,50 @@
 // let pr =document.querySelector('p')
 // pr.style.color = 'red';
 
-let content =document.querySelector('.content')
-content.style.color = 'red';
+// let content =document.querySelector('.content')
+// content.style.color = 'red';
 
-let pr = document.querySelectorAll('p')
-pr.forEach(pr =>{
-    pr.style.color = 'red'
-})
+// let pr = document.querySelectorAll('p')
+// pr.forEach(pr =>{
+//     pr.style.color = 'red'
+// })
 
 // let contents = document.querySelectorAll('.content')
 // .Array.contents.forEach(content =>{
 //     content.style.color = 'blue'
 // })
 
-let costom_query = document.querySelector(
-  "#header"
-);
-costom_query.style.color = "green";
-costom_query.style.fontSize = '40px'
+// let costom_query = document.querySelector(
+//   "#header"
+// );
+// costom_query.style.color = "green";
+// costom_query.style.fontSize = '40px'
 
+
+
+// let lists = document.querySelectorAll('li');
+// lists.forEach(lists =>{
+//     console.log(lists.innerText);
+// })
+
+// let lists = document.querySelectorAll('li')
+// lists.forEach(list => {
+    // list.innerText += " new"
+    // list.innerHTML += "<span class='badge bg-danger'>new</span>"
+    // list.textContent += ' new'
+// })
+   
+
+// let list = document.querySelector('li')
+// console.log(list.innerText);
+// console.log(list.textContent);
+
+
+
+let lists = document.querySelectorAll('li')
+
+lists.forEach(list =>{
+    if(list.textContent.includes(' new')){
+        list.innerHTML += "<span class='badge bg-danger'>new</span>"
+    }
+})
