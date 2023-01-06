c164b6b03ed3152dfd5923e11450d784f104f911
Hossein <hosseindozandeh799@gmail.com>
2023-01-06 14:46:08 +0330

    055

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-let pepole = [
-    {name: 'Hossein' , new: true},
-    {name: 'Amir' , new: false},
-    {name: 'Ali' , new: false},
-    {name: 'Amir Reza' , new: true},
-]
+// let header = document.querySelector('#header')
 
-function create_navbar(pepole) {
-    let ul = document.querySelector('ul')
-    ul.innerHTML=''
-pepole.forEach( p =>{
-   if(p.new){
-    ul.innerHTML += `<li>${p.name} <span class='badge bg-danger'>new</span></li>`
-   }else{
-    ul.innerHTML += `<li>${p.name}</li>`
-   }
+// console.log(header.getAttribute('id'));
+// header.setAttribute('title' , 'my header title')
+
+let links = document.querySelectorAll('a')
+links.forEach(link =>{
+    href = link.getAttribute('href')
+    link.setAttribute('href', href.replace('http://', 'https://'))
+    link.setAttribute('data-portocol' , 'ssl')
 })
-}
-create_navbar(pepole)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// !054
+// let pepole = [
+//     {name: 'Hossein' , new: true},
+//     {name: 'Amir' , new: false},
+//     {name: 'Ali' , new: false},
+//     {name: 'Amir Reza' , new: true},
+// ]
+
+// function create_navbar(pepole) {
+//     let ul = document.querySelector('ul')
+//     ul.innerHTML=''
+// pepole.forEach( p =>{
+//    if(p.new){
+//     ul.innerHTML += `<li>${p.name} <span class='badge bg-danger'>new</span></li>`
+//    }else{
+//     ul.innerHTML += `<li>${p.name}</li>`
+//    }
+// })
+// }
+// create_navbar(pepole)
